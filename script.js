f30d9cdb81e599933d620d7207b24bc2398c0506
FernandoJunior7 <fernando.junior1907@gmail.com>
2024-05-14 09:18:56 -0300

    Add logic to human choice

--- script.js
+++ script.js
@@ -6,4 +6,21 @@ function getComputerChoice() {
 
 let computerChoice = elements[getComputerChoice()];
 
-console.log(computerChoice);
+function getHumanChoice() {
+    let isChoiceValid;
+    let choice;
+
+    do {
+        isChoiceValid = true;
+        choice = prompt('O - Pedra, 1 - Papel, 2 - Tesoura');
+
+        if (choice < 0 || choice > 2) {
+            isChoiceValid = false;
+        }
+
+    } while (!isChoiceValid)
+
+    return choice;
+}
+
+let humanChoice = getHumanChoice();
