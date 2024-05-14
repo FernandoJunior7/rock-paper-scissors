581d21fc4ae735d4dc15ada662fe247705e67d6a
FernandoJunior7 <fernando.junior1907@gmail.com>
2024-05-14 10:26:02 -0300

    Add logic to play single round

--- script.js
+++ script.js
@@ -17,14 +17,9 @@ function getHumanChoice() {
         isChoiceValid = true;
         choice = prompt('Pedra, Papel ou Tesoura?').toLowerCase();
 
-        console.log(choice === 'pedra');
-
         if (choice !== 'pedra' && choice !== 'papel' && choice !== 'tesoura') {
             isChoiceValid = false;
         }
-
-        console.log(`Choice: ${choice}`);
-        console.log(`isChoiceValid: ${isChoiceValid}`);
 
     } while (!isChoiceValid)
 
@@ -32,3 +27,52 @@ function getHumanChoice() {
 }
 
 let humanChoice = getHumanChoice();
+
+function playRound(humanChoice, computerChoice) {
+
+    if (computerChoice === humanChoice) {
+        console.log(`Houve um empate. Ambos escolheram ${humanChoice}`);
+        return;
+    }
+    
+    switch (humanChoice) {
+        case 'pedra':
+            if (computerChoice === 'papel') {
+                console.log(`O computador venceu essa rodada! ${computerChoice} enrola a ${humanChoice}`);
+                computerScore++;
+            }
+            if (computerChoice === 'tesoura') {
+                console.log(`Você venceu essa rodada! ${humanChoice} amassa a ${computerChoice}`);
+                humanScore++;
+            }    
+            break;
+
+        case 'papel':
+            if (computerChoice === 'tesoura') {
+                console.log(`O computador venceu essa rodada! ${computerChoice} corta o ${humanChoice}`);
+                computerScore++;
+            }
+            if (computerChoice === 'pedra') {
+                console.log(`Você venceu essa rodada! ${humanChoice} enrola a ${humanChoice}`);
+                humanScore++;
+            }
+            break;
+
+        case 'tesoura':
+            if (computerChoice == 'pedra') {
+                console.log(`O computador venceu essa rodada! ${computerChoice} amassa a ${humanChoice}`);
+                computerScore++;
+            }
+            if (computerChoice == 'tesoura') {
+                console.log(`Você venceu essa rodada! ${humanChoice} corta o ${computerChoice}`);
+                humanScore++;
+            }
+            break;
+    
+        default:
+            console.log('Erro inesperado, não era pra cair aqui');
+            break;
+    }
+}
+
+playRound(humanChoice, computerChoice);
